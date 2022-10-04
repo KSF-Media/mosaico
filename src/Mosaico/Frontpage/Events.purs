00a343ee0cb7dc309d2fc6e64c489a56a0889333
Kari Pahula <kaol@iki.fi>
2022-10-04 15:54:24 +0300

    Only capture frontpage clicks on intra site links

--- src/Mosaico/Frontpage/Events.purs
+++ src/Mosaico/Frontpage/Events.purs
@@ -3,15 +3,17 @@ module Mosaico.Frontpage.Events where
 import Prelude
 
 import Data.Maybe (Maybe(..), maybe)
+import Data.String as String
 import Effect (Effect)
-import React.Basic.Events (EventHandler)
-import React.Basic.DOM.Events (capture, target)
+import React.Basic.Events (EventHandler, handler)
+import React.Basic.DOM.Events (nativeEvent)
 import Web.DOM (Node)
 import Web.DOM.Element as Element
 import Web.DOM.Node as Node
+import Web.Event.Event as Event
 
 onFrontpageClick :: (String -> Effect Unit) -> EventHandler
-onFrontpageClick setRoute = capture target $ \tgt -> do
+onFrontpageClick setRoute = handler nativeEvent $ \event -> do
   let findAnchor :: Node -> Effect (Maybe String)
       findAnchor node = do
         let continue = maybe (pure Nothing) findAnchor =<< Node.parentNode node
@@ -21,7 +23,10 @@ onFrontpageClick setRoute = capture target $ \tgt -> do
             maybe (pure Nothing) (Element.getAttribute "href") (Element.fromNode node)
           _ ->
             continue
-  maybeHref <- maybe (pure Nothing) findAnchor $ Node.fromEventTarget tgt
+      tgt = Event.target event
+  maybeHref <- maybe (pure Nothing) findAnchor $ Node.fromEventTarget =<< tgt
   case maybeHref of
-    Just href -> setRoute href
+    Just href | String.take 1 href == "/" -> do
+      Event.preventDefault event
+      Event.stopPropagation event
     _ -> pure unit
