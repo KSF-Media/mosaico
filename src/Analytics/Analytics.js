514ae6e5c7df8602d5ce1289730bc27991658c1b
Max Fahler <max.fahler@ksfmedia.fi>
2022-11-28 12:55:31 +0100

    restore datalayer resets

--- src/Analytics/Analytics.js
+++ src/Analytics/Analytics.js
@@ -1,5 +1,8 @@
 export function _pushToDataLayer(metadata) {
     window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push(function() {
+        this.reset();
+      })
     let push_data = { event: "page_data" };
     push_data.title = metadata.title
     push_data.publishingTime =  metadata.publishingTime
@@ -17,6 +20,9 @@ export function _pushToDataLayer(metadata) {
 
 export function _sendPageView() {
     window.dataLayer = window.dataLayer || []
+    window.dataLayer.push(function() {
+        this.reset();
+      })
     let push_data = { event: "page_data", pageUrl: window.location.href, pageTitle: document.title}
     dataLayer.push(push_data)
 }
