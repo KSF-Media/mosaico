5a27390185f70e2e2fc178f8d77cb2c0ae1a0d16
Jaakko Hannikainen <jaakko.hannikainen@bytecraft.fi>
2023-02-07 14:49:42 +0200

    Remove whitelist from cors proxy

--- src/Cors.purs
+++ src/Cors.purs
@@ -17,27 +17,17 @@ import Partial.Unsafe (unsafePartial)
 protocolRegex :: Regex
 protocolRegex = unsafePartial $ fromJust $ hush $ regex """^(https?:)?//""" noFlags
 
-isWhitelisted :: String -> Maybe String
-isWhitelisted url = case urlWithoutProtocol of
-    "www.riddle.com/embed/files/js/embed.js" -> Just urlAsHttps
-    "platform.instagram.com/en_US/embeds.js" -> Just urlAsHttps
-    "www.instagram.com/embed.js" -> Just urlAsHttps
-    "platform.twitter.com/widgets.js" -> Just urlAsHttps
-    "embed.tt.se/v10/tt-widget.js" -> Just urlAsHttps
-    "player-v2.yle.fi/embed.js" -> Just urlAsHttps
-    "web.keesing.com/pub/portal/v2.20.7/dist/main-bundle.js" -> Just urlAsHttps
-    _ -> Nothing
+toHttps :: String -> String
+toHttps url = urlAsHttps
   where urlWithoutProtocol = replace protocolRegex "" url
         urlAsHttps = "https://" <> urlWithoutProtocol
 
 fetchUrl :: String -> Aff (Either String String)
-fetchUrl resource = case isWhitelisted resource of
-    Just url -> do
-        driver <- liftEffect getDriver
-        corsResponse <- AX.get driver AX.string url
-        case corsResponse of
-            Left err -> pure $ Left $ "Failed to fetch a resource through the CORS proxy" <> AX.printError err
-            Right response
-                | (StatusCode 200) <- response.status -> pure $ Right response.body
-                | (StatusCode s) <- response.status -> pure $ Left $ "Unexpected HTTP status: " <> show s
-    Nothing -> pure $ Left "URL not whitelisted"
+fetchUrl url = do
+    driver <- liftEffect getDriver
+    corsResponse <- AX.get driver AX.string (toHttps url)
+    case corsResponse of
+        Left err -> pure $ Left $ "Failed to fetch a resource through the CORS proxy" <> AX.printError err
+        Right response
+            | (StatusCode 200) <- response.status -> pure $ Right response.body
+            | (StatusCode s) <- response.status -> pure $ Left $ "Unexpected HTTP status: " <> show s
