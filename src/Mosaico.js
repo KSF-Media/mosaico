a86ad31f46ec1d53b8029ea823d29ef7eca1f51e
Daniel Clarke <daniel.clarke@ksfmedia.fi>
2023-03-15 13:45:49 +0000

    Fetch ads from Mosaico only

--- src/Mosaico.js
+++ src/Mosaico.js
@@ -1,16 +1,10 @@
 export function refreshAdsImpl(slotArray) {
-  if (typeof window.googletag.pubads === "function") {
-    window.googletag
-      .pubads()
-      .getSlots()
-      .map((s) => {
-	if (slotArray.includes(s.getSlotElementId())) {
-	  window.googletag.pubads().refresh([s]);
-	  // console.log("Refreshing ad: " + s.getSlotElementId());
-	}
-      });
-  }
-};
+  setTimeout(() => {
+    loadAds()
+    console.log("loadAds from Mosaico.js: ", loadAds)
+  }, 500);
+}
+
 export function sentryDsn_() {
   return process.env.SENTRY_DSN;
 };
