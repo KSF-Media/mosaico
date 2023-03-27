4fde1c24a3c6eb800fbd5f0f7716ba9ddd783cf8
Jaakko Hannikainen <jaakko.hannikainen@bytecraft.fi>
2023-03-27 10:15:24 +0300

    Try hiding breaking banners from google

--- src/Mosaico/BreakingNews.purs
+++ src/Mosaico/BreakingNews.purs
@@ -4,6 +4,7 @@ module Mosaico.BreakingNews
   )
   where
 
+import Foreign.Object (singleton)
 import React.Basic (JSX)
 import React.Basic.DOM as DOM
 
@@ -13,5 +14,6 @@ render :: BreakingNewsProps -> JSX
 render {content} =
   DOM.div
     { className: "mosaico--breaking-news"
+    , _data: singleton "nosnippet" "true"
     , dangerouslySetInnerHTML: {__html: content }
     }
